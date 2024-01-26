8dfd1a46cc646b8812779ea01a6e340ccaa2a8ca
rinkal1122 <130449228+rinkal1122@users.noreply.github.com>
2024-01-26 14:18:49 +0530

    Add files via upload
    que6

--- Module9/QUE6.java
+++ Module9/QUE6.java
@@ -2,7 +2,9 @@ package Module9;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.remote.DesiredCapabilities;
 /*
  *  Write an Appium Program to connect with emulator and open APIdemo.
  * app application on your emulator
@@ -10,18 +12,32 @@ import java.net.URL;
  */
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class QUE6 {
 	@Test
 public void emulator() throws MalformedURLException, InterruptedException {
-
-		UiAutomator2Options options=new UiAutomator2Options();
-		options.setDeviceName("OPPO CPH2531");
-		options.setApp("D:\\\\APK file\\\\apk file\\\\Apidemos-debug.apk");
-		AndroidDriver driver=new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
-		Thread.sleep(3000);
-		driver.quit();
+		
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability("deviceName", "OPPO CPH2531");
+		cap.setCapability("udid", "KR69JRD6HEDQ99EQ");
+		cap.setCapability("platformName","Android");
+		cap.setCapability("platformverson","14.0");
+		cap.setCapability("appPackage", "io.appium.android.apis");
+		cap.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+		cap.setCapability("automationName", "UIAutomator2");
+		
+				URL url=new URL("http://127.0.0.1:4723/");
+				
+				AppiumDriver driver=new AppiumDriver(url,cap);
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		 Thread.sleep(2000);
+		 
+		 
+		 
+		 
+		 
 }
 }
